8d5bcd3069f9161e3395368ebc16be8d124e3828
ka11inis <leka11inis@gmail.com>
2020-03-12 21:13:19 +0200

    Εισαγωγή του αλγόριθμου γρήγορης ταξινόμησης  --  Add quick sort algorithm

---
+++ sort/Quick Sort/quick_sort.c
@@ -0,0 +1,99 @@
+//Αλγόριθμος Γρήγορης Ταξινόμησης 
+//Quicksort Algorithm
+
+//Χρονική πολυπλοκότητα 
+//Time complexity 
+//Best    = nlogn
+//Average = nlogn
+//Worst   = n^2
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Μέγεθος του πίνακα
+//Array length
+#define ARR_LEN 1000000  //1.000.000   [0..999999]
+
+//Στη swap συνάρτηση αντιμετατίθενται δύο μεταβλητές
+//The swap function, take two variables and are swapped
+//a=b, b=a
+void swap(int* a, int* b){
+    int tmp = *a; *a = *b; *b = tmp;
+}
+
+//Η συνάρτηση partition παίρνει το τελευταίο στοιχείο σαν pivot
+//το και ταξινομεί τα μικρότερα στοιχεία αριστερά  του και τα 
+//μεγαλύτερα στοιχεία δεξιά του
+//The function partition takes the last element as a pivot 
+//and puts all the smaller elements on the left and all the 
+//larger elements on the right.
+//[9,4,6,7,6,0,4]  ->  [0,4,4,9,6,7,6] 
+int partition(int arr[], int lo, int hi){
+    
+    //Για pivot επιλέγει το στοιχείο στη δεξιότερη θέση
+    //Pivot (item in the most right position)
+    int pivot = arr[hi];
+    int small = (lo - 1);//Η θέση του μικρότερου στοιχείου 
+    
+    //Για κάθε ένα στοιχείο του πίνακα κάνε
+    //For each element of the table do
+    for (int i = lo; i <= hi - 1; i++)
+        //Εάν το στοιχείο που ελέγχουμε (arr[i]) είναι μικρότερο ή ίσο 
+        //του pivot τότε τοποθετείτε αριστερά του υποπίνακα 
+        //If the element we control (arr[i]) is less than to the pivot
+        //then place it to the left of the subtable 
+        if ( arr[i] <= pivot ){
+            small++;
+            swap( &arr[small], &arr[i] );
+        }
+    //Αντιμετάθεση του αριθμού ελέγχου (pivot) 
+    //στη θέση του διαχωρισμού του πίνακα 
+    swap( &arr[small + 1], &arr[hi]);
+    //Επιστέφει τι θέση του πίνακα που αριστερά του έχει 
+    //τις μικρότερες τιμές και δεξιά του τις μεγαλύτερες 
+    //του αριθμου ελέγχου (pivot)
+    return (small + 1);
+}
+
+//Ο αναδρομικός αλγόριθμος τις quicksort
+void quick_sort(int* arr, int lo, int hi){
+    //Όσο η "lo" είναι μικρότερη τις "hi"
+    //While the "lo" is less than "hi"
+    if (lo < hi){
+        //Επιστρέφει το σημείο διαχωρισμού του πίνακα
+        //Returns the separation point of the table
+        int pi = partition(arr, lo, hi);
+        //Ο πρώτος υποπίνακας [lo..pivot-1]
+        //The first subtable  [lo..pivot-1]
+        quick_sort(arr, lo, pi - 1);
+        //Ο δεύτερος υποπίνακας [pivot..hi]
+        //The second subtable   [pivot..hi]
+        quick_sort(arr, pi + 1, hi);
+    }
+}
+
+//Εκτυπώνει τον πίνακα
+//It prints the table
+void print_array(int* arr){ 
+    for (int i=0; i < ARR_LEN; i++){
+        printf("%d ", arr[i] );
+    }
+}
+
+//Γεμίζει τον πίνακα με τυχαίους αριθμούς  [0..10000]
+//It fills the table with random numbers 
+void fill_array(int* arr){
+    for (int i=0; i < ARR_LEN; i++){
+        arr[i] = rand() % ( 10000 );
+    }
+}
+
+//
+int main (int argc, char* argv){
+    int arr[ARR_LEN];
+    fill_array( &arr[0] );
+    //print_array( &arr[0] );   //Εκτυπώνει πριν την ταξινόμηση
+    quick_sort( &arr[0], 0, ARR_LEN - 1);
+    //print_array( &arr[0] );  //Εκτυπώνει μετά την ταξινόμηση
+    
+}
