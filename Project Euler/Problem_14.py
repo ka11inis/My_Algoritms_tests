1c076a6c27ef49bb664e44fb2493f69f4029af69
ka11inis <leka11inis@gmail.com>
2020-03-20 22:34:34 +0200

    Εισαγωγή του προβλήματος 14 του projecteuler.net  --  Add the problem 14 from the projecteuler.net

---
+++ Project Euler/Problem_14.py
@@ -0,0 +1,35 @@
+#Euler Project
+#Problem 13
+#slow, but works
+
+import time
+start = time.time()
+
+number = 837799
+cnt = 0
+max_cnt=0
+max_number=0
+
+for number in range(2,1000000):
+        
+        if max_cnt < cnt: 
+                max_cnt = cnt
+                max_number = number -1
+        cnt = 1
+
+        while number > 1:
+                
+                cnt = 1 + cnt
+                if number == 1:
+                        pass
+                elif number % 2 == 0:
+                        number = number/2
+                        #print number
+                else:
+                        number = 3 * number + 1
+                        #print number
+
+print ("count ",max_cnt)
+print ("number ",max_number)
+end = time.time()
+print (end - start)
