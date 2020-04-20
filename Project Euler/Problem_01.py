68b8c52af187075792cfe51a55059e28b3ccc300
ka11inis <leka11inis@gmail.com>
2020-04-20 21:39:52 +0300

    Εισαγωγή του προβλήματος 01 του projecteuler.net  --  Add the problem 01 from the projecteuler.net

--- Project Euler/Problem_01.py
+++ Project Euler/Problem_01.py
@@ -1,5 +1,15 @@
+#Euler Project
+#Problem 01
+
+import time
+start = time.time()
+
 a=0
 for i in range(1,1000):
     if (i%5 ==0 or i%3==0):
         a=a+i
 print (a)
+
+
+end = time.time()
+print (end - start)
