707a5ddc3d1aaf0a2d8ce41de0cc5e875379a8d4
ka11inis <leka11inis@gmail.com>
2020-04-21 20:08:59 +0300

    Εισαγωγή του προβλήματος 02 του projecteuler.net  --  Add the problem 02 from the projecteuler.net

--- Project Euler/Problem_02.py
+++ Project Euler/Problem_02.py
@@ -1,7 +1,11 @@
+#Euler Project
+#Problem 01
+
+import time
+start = time.time()
 
 res = [1,1,2]
 i=3
-
 while True:
     res.append(res[i-2] + res[i-1])
     if (res[i] > 4000000 ):
@@ -10,3 +14,8 @@ while True:
 res.pop()
 odd_fib = [i for i in res if i&1]
 print(sum( odd_fib))
+
+
+
+end = time.time()
+print ("{:.15f}".format(end - start))
